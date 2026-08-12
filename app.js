64c42f2ee4a9df49a8a6af756f4972705f0072c4
Darshannikam9767 <darshannikam64@gmail.com>
2026-08-12 12:18:03 +0530

    added gitignore to prevent the secrets from exposing

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 import express from "express"
+import {nanoid} from "nanoid"
+
 const app = express()
 const port = 3000
 
 
-app.listen(port, () =>{
-    console.log(`backend is running on port = ${port}`);
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.get("/health", (req, res)=>{
+    res.send(`BACKEND IS HEALTHY & RUNNING ON PORT = '${port}'`)
 })
 
-app.get("/", (req, res)=>{
-    res.send(`BACKEND IS RUNNING ON PORT = '${port}'`)
+app.post("/api/create",(req, res) => {
+    const {url} = req.body
+    console.log(url);
+    res.send(nanoid(7))
+    
+})
+
+app.listen(port, () =>{
+    console.log(`backend is running on port = ${port}`);
 })
