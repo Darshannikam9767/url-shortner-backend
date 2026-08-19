8034b981e91af85d188c11461cb561809607c572
Darshannikam9767 <darshannikam64@gmail.com>
2026-08-19 13:56:31 +0530

    fixed the issue occures while creating custom url

--- src/controller/shortUrl.controller.js
+++ src/controller/shortUrl.controller.js
@@ -18,19 +18,41 @@ export const createShortUrl = wrapAsync(async (req, res) => {
 })
 
 export const createCustomShortUrl = wrapAsync(async (req, res) => {
-    console.log("in custom url route....");
-    
-    if(!req.user) throw new Error("Login first to create custom url")
+    console.log("in custom url route....")
+
+    if (!req.user) {
+        return res.status(401).json({
+            message: "Login first to create custom url"
+        })
+    }
 
     const { url, slug } = req.body
-    console.log(`url = ${url} \n custom url = ${slug}`);
-    
-    const short_url = await createShortUrlWithUser(url,req.user._id,slug)
-    res.status(200).json({
-        short_url: process.env.APP_URL + short_url
-    })
-}
-)
+
+    console.log(`url = ${url} \ncustom url = ${slug}`)
+
+    try {
+        const short_url = await createShortUrlWithUser(
+            url,
+            req.user._id,
+            slug
+        )
+
+        return res.status(200).json({
+            short_url: process.env.APP_URL + short_url
+        })
+
+    } catch (error) {
+
+        if (error.code === 11000) {
+            return res.status(409).json({
+                message: "Custom URL already exists"
+            })
+        }
+
+        throw error
+    }
+})
+
 export const redirectFromShortUrl = wrapAsync(async (req, res) => {
 
     const { id } = req.params
